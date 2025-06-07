a11763bd39ab8658709ba244eb219cfe5b9a691a
YacineBoukliff <yacine69800@live.fr>
2025-06-07 20:03:56 +0200

    Debut du refacto

--- app/api/equipes/route.ts
+++ app/api/equipes/route.ts
@@ -1,9 +1,12 @@
 import { auth } from "@/auth";
 import { prisma } from "@/prisma";
+import { headers } from "next/headers";
 import {  NextResponse } from "next/server";
 
 export async function GET() {
-  const session = await auth();
+  const session = await auth.api.getSession({
+    headers: await headers() 
+})
 
   if (!session?.user?.id)
     return NextResponse.json(
