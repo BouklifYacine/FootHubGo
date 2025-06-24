4f2d436cda1140299bb8bffece2801e35595a41c
YacineBoukliff <yacine69800@live.fr>
2025-06-24 16:13:11 +0200

    Reprise du projet on modifier les événements par l'entraineur et on passe a la toute fin du MVP avant déploiement

--- app/api/evenements/[id]/route.ts
+++ app/api/evenements/[id]/route.ts
@@ -1,80 +1,52 @@
-
-import { ModifierEvenementSchema } from "@/features/evenements/schemas/ModificationEvenementsSchema";
+import { auth } from "@/auth";
 import { prisma } from "@/prisma";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
   params: { id: string };
 }
 
-const userId = "Lm9B28oAI4ShwdFuVDgGevzPedyHcppb";
-
-
-export async function PATCH(request: NextRequest, { params }: Props) {
+export async function GET(request: NextRequest, { params }: Props) {
   const { id } = await params;
-  const body = await request.json();
-
-  const validation = ModifierEvenementSchema.safeParse(body);
-  if (!validation.success) {
-    return NextResponse.json(
-      { message: validation.error.errors[0].message },
-      { status: 400 }
-    );
-  }
-  const DataaJour = validation.data;
 
   if (!id)
     return NextResponse.json(
-      { message: "Id de l'évenement manquant" },
+      { message: "Id de l'évenement inexistant ou incorrect" },
       { status: 400 }
     );
 
-  if (!userId) {
-    return NextResponse.json(
-      { message: "Vous devez être connecté pour créer un événement." },
-      { status: 401 } // 401 Unauthorized est plus approprié ici
-    );
-  }
-
-  const membreEquipe = await prisma.membreEquipe.findFirst({
-    where: { userId: userId },
-    select: { role: true, equipeId: true },
+  const session = await auth.api.getSession({
+    headers: await headers(),
   });
 
-  if (!membreEquipe || membreEquipe.role !== "ENTRAINEUR") {
-    return NextResponse.json(
-      {
-        message:
-          "Vous devez être entraîneur d'une équipe pour créer des événements.",
-      },
-      { status: 403 }
-    );
-  }
+  const userId = session?.user.id;
 
-  const Evenementexistant = await prisma.evenement.findUnique({
+  if (!userId)
+    return NextResponse.json(
+      { message: "Vous devez vous connectez" },
+      { status: 401 }
+    );
+
+  const membreEquipe = await prisma.membreEquipe.findUnique({
+    where: { id: userId },
+    select: { role: true },
+  });
+
+  if (!membreEquipe)
+    return NextResponse.json({
+      message: "Vous devez etre membre de l'équipe pour voir ses événements ",
+    });
+
+  const evenementUnique = await prisma.evenement.findUnique({
     where: { id },
   });
-  if (!Evenementexistant) {
-    return NextResponse.json(
-      { message: "Événement introuvable." },
-      { status: 404 }
-    );
-  }
-  if (Evenementexistant.equipeId !== membreEquipe.equipeId) {
-    return NextResponse.json(
-      {
-        message: "Vous ne pouvez pas modifier un événement d'une autre équipe.",
-      },
-      { status: 403 }
-    );
-  }
 
-  const Evenementajour = await prisma.evenement.update({
-    where: { id },
-    data: DataaJour,
-  });
-  return NextResponse.json(
-    { message: "Événement mis à jour.", evenement: Evenementajour },
-    { status: 200 }
-  );
+  if (!evenementUnique)
+    return NextResponse.json(
+      { message: "Cet evenement n'existe pas" },
+      { status: 400 }
+    );
+
+    return NextResponse.json(evenementUnique)
 }
