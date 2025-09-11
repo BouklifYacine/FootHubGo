9295a709b1dcdefdfd765c66a9df40dbb71d7c79
YacineBoukliff <yacine69800@live.fr>
2025-09-11 13:17:23 +0200

    Composant classement meilleurs buteurs et passeurs fini

--- features/accueil/components/LeaderboardTeam.tsx
+++ features/accueil/components/LeaderboardTeam.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Logo from "@/public/Logo_Manchester_City_2016.svg";
 import Image from "next/image";
 import { PlayerLeaderboard } from "../interfaces/InterfaceApiAccueil";
+import { Badge } from "@/components/ui/badge";
 
 interface Props {
   TopAssists : PlayerLeaderboard[] | undefined,
@@ -16,18 +17,18 @@ function LeaderboardTeam({TopAssists,TopScorers} : Props) {
 
   const [ShowGoals, setShowGoals] = useState(!false)
   return (
-    <div className="w-full max-w-2xl rounded-xl bg-red-500 p-4 border border-gray-200">
+    <div className="w-full max-w-2xl rounded-xl  p-4 border border-gray-200 shadow shadow-blue-500">
       <div className="flex justify-between  ">
-        <p>{ShowGoals? "Top buteurs" : "Top Passeurs"}</p>
+        <p className="text-lg tracking-tighter font-bold">{ShowGoals? "Classement Buteurs" : "Classement Passeurs"}</p>
         <div className="flex items-center gap-2">
-          <Switch id="airplane-mode" checked={ShowGoals} onCheckedChange={setShowGoals} />
-          <Label htmlFor="airplane-mode">{ShowGoals? "Buteur" : "Passeurs"}</Label>
+          <Switch  checked={ShowGoals} onCheckedChange={setShowGoals} />
+          <Label className="text-lg tracking-tighter" >{ShowGoals? "Buteur" : "Passeurs"}</Label>
         </div>
       </div>
 
   {ShowGoals ? (
   TopScorers?.map((a) => (
-    <div key={a.id} className="flex justify-between mt-4 items-center">
+    <div key={a.id} className="flex justify-between mt-5 items-center">
       <div className="flex gap-2">
         <Image
           alt="logo joueur"
@@ -37,11 +38,15 @@ function LeaderboardTeam({TopAssists,TopScorers} : Props) {
           className="rounded-full"
         />
         <div className="flex flex-col">
-          <p>{a.playerName}</p>
-          <p>{a.playerPosition}</p>
+       <div>
+           <p className="tracking-tight font-medium">{a.playerName}</p>
+       </div>
+        <div className="flex items-center">
+           <Badge className="tracking-tight">{a.playerPosition}</Badge>
+        </div>
         </div>
       </div>
-      <p>{a._sum.buts} {a._sum.buts != null && a._sum.buts > 1 ? "Buts" : "But"}</p>
+      <p className="tracking-tighter text-lg font-medium">{a._sum.buts} {a._sum.buts != null && a._sum.buts > 1 ? "Buts" : "But"}</p>
     </div>
   ))
 ) : (
@@ -56,11 +61,15 @@ function LeaderboardTeam({TopAssists,TopScorers} : Props) {
           className="rounded-full"
         />
         <div className="flex flex-col">
-          <p>{a.playerName}</p>
-          <p>{a.playerPosition}</p>
+       <div>
+           <p className="tracking-tight font-medium">{a.playerName}</p>
+       </div>
+        <div className="flex items-center">
+           <Badge className="tracking-tight ">{a.playerPosition}</Badge>
+        </div>
         </div>
       </div>
-      <p>{a._sum.passesdecisive} {a._sum.passesdecisive != null && a._sum.passesdecisive > 1 ? "Passes" : "Passe"}</p>
+      <p className="tracking-tighter text-lg font-medium ">{a._sum.passesdecisive} {a._sum.passesdecisive != null && a._sum.passesdecisive > 1 ? "Passes" : "Passe"}</p>
     </div>
   ))
 )}
